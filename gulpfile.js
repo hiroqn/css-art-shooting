fb0a1d18bfeafdb5988b7a474501ece68f17c505
hiroqn <hiroqn1008@gmail.com>
2015-04-03 23:29:58 +0900

    add deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -45,3 +45,43 @@ gulp.task('serve', ['jade', 'styles'], function (cb) {
   gulp.watch(['app/**/*.jade'], ['jade',  browserSync.reload]);
   gulp.watch(['app/**/*.less'], ['styles', browserSync.reload]);
 });
+
+
+// Deployment to GitHub using git.
+gulp.task('deploy', ['default'], function (done) {
+  runSequence('deploy:commit', 'deploy:push', done);
+});
+
+// Commit changes to production build locally.
+gulp.task('deploy:commit', function (done) {
+  var msg, im = process.argv.indexOf('-m');
+  if (im >= 0) {
+    msg = process.argv.slice(im, im + 2)[1];
+  }
+  msg = msg || 'Production build';
+  var cmd = ["git add .", "git commit -m '" + msg + "'", "git status"];
+  exec(cmd.join(' && '), {cwd: path.join(process.cwd(), 'dist')}, function (err, stdout, stderr) {
+    if (!/working directory clean/m.test(stdout)) {
+      console.log('--- ERROR log ---\n' + stderr);
+      console.log('--- STDOUT log ---\n' + stdout);
+      throw err || new Error('deploy:commit');
+    }
+    done();
+  });
+});
+
+// Push changes to remote (actual deployment).
+gulp.task('deploy:push', function (done) {
+  var cwd = path.join(process.cwd(), 'dist');
+  var args = ['push', '-u', 'origin', 'gh-pages'];
+  spawn('git', args, {cwd: cwd, stdio: 'inherit'}).on('close', function (code) {
+    if (code !== 0) {
+      throw new Error('deploy:push exit code is ' + code);
+    }
+    done();
+  });
+});
+
+gulp.task('default', ['jade', 'styles'], function (cb) {
+  cb();
+});
